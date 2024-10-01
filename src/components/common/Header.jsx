424bb32f8c3fe34dae71bedff4b974e431326157
tspartner21 <tspartner21@gmail.com>
2024-10-01 16:00:47 +0900

    메인 레이아웃

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,32 @@
+import { FaEnvelope, FaInstagramSquare, FaYoutube } from "react-icons/fa";
+export default function Header() {
+  return (
+    <header className='header'>
+     <h1>
+        <a href='/'>ALPACO</a>
+     </h1>
+     <nav>
+        <ul className="gnb">
+            <li><a href="#">MEMBERS</a></li>
+            <li><a href="#">GALLERY</a></li>
+            <li><a href="#">YOUTUBE</a></li>
+            <li><a href="#">CONTACT</a></li>
+            <li><a href="#">POSTS</a></li>
+        </ul>
+
+        <ul className="sns">
+          <li>
+          <FaYoutube />
+          </li>
+          <li>
+            <FaInstagramSquare/>
+          </li>
+          <li>
+            <FaEnvelope/>
+          </li>
+
+        </ul>
+     </nav>
+    </header>
+  )
+}
