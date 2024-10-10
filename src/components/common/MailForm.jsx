01a856025554bad2d1c4377d68f54b1681dc03e4
tspartner21 <tspartner21@gmail.com>
2024-10-10 17:35:54 +0900

    form영역 구조 추가

--- src/components/common/MailForm.jsx
+++ src/components/common/MailForm.jsx
@@ -1,44 +1,48 @@
 export default function MailForm(){
-    return <article className='mailForm'>
-              <form>                
-                <div className="formBox">
-                    <form>
-                        <div className="upper">
-                            <span>
-                                <label htmlFor="uName">Name</label>
-                                <input type="text" id="uName" placeholder="Leave your name" />
-                            </span>
-                            <span>
-                                <label htmlFor="uMail">E-Mail</label>
-                                <textarea type="text" id="uMail" placeholder="Leave your email"></textarea>
-                            </span>
-                        </div>
-                        <div className="lower">
-                            <label htmlFor="msg">Message</label>
-                            <textarea name="msg" id="msg" placeholder="Leav your message">
-                            </textarea>
-                        </div>
+return (
+        <article className='mailForm'>
 
-                        {/*전송 , 취소 버튼 그룹 */}
-                        <nav className="btnSet">
-                            <input type="reset" value="Cancel" />
-                            <input type="submit" value="Send" />
-                        </nav>
-                    </form>
-                </div>
-                <div className='info'>
-					<h2>Information</h2>
-					<p>
-						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate reprehenderit laudantium cupiditate qui? Possimus sint eum
-						obcaecati ut cum ea, praesentium temporibus, quos vel beatae vero esse quisquam expedita animi.
-					</p>
-					<br />
-					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia, sequi ipsum. Deleniti nesciunt rerum ex!</p>
-				</div>
-    
-              </form>
-               
-
+            <form>                
+            <div className="formBox">
                 
-            </article>;
+                <form>
+                    <div className="upper">
+                        <span>
+                            <label htmlFor="uName">Name</label>
+                            <input type="text" id="uName" placeholder="Leave your name" />
+                        </span>
+                        <span>
+                            <label htmlFor="uMail">E-Mail</label>
+                            <input type="text" id="uMail" placeholder="Leave your email" />
+                        </span>
+                    </div>
+                    <div className="lower">
+                        <label htmlFor="msg">Message</label>
+                        <textarea name="msg" id="msg" placeholder="Leav your message">
+                        </textarea>
+                    </div>
+
+                    {/*전송 , 취소 버튼 그룹 */}
+                    <nav className="btnSet">
+                        <input type="reset" value="Cancel" />
+                        <input type="submit" value="Send" />
+                    </nav>
+                </form>
+            </div>
+            <div className='info'>
+                <h2>Information</h2>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate reprehenderit laudantium cupiditate qui? Possimus sint eum
+                    obcaecati ut cum ea, praesentium temporibus, quos vel beatae vero esse quisquam expedita animi.
+                </p>
+                <br />
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia, sequi ipsum. Deleniti nesciunt rerum ex!</p>
+            </div>
+
+            </form>
+            
+
+            
+        </article>
+    );
 }
