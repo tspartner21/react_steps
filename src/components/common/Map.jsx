6c57963f9334893cfa81059c6960bbc7ebbaf265
tspartner21 <tspartner21@gmail.com>
2024-10-11 14:14:42 +0900

    참고객체에 정적인 데이터 활용시 비구조활당을 통한 코드 축약

--- src/components/common/Map.jsx
+++ src/components/common/Map.jsx
@@ -11,7 +11,9 @@ export default function Map(){
     //지도 인스턴스가 담길 빈 참조객체 생성
     const ref_instMap = useRef(null);
 
-    const ref_info = useRef([ //개체를 미리 생성함
+    //current 쓰기 불편한 경우
+    //참조 객체에 담기는 값이 추후 동적으로 변경될 일이 없으면 아래처럼 비구조화할당시 변수명을 바꿔치기 하는 식으로 사용가능
+    const {current : ref_info} = useRef([ //개체를 미리 생성함
         {
         title : 'COEX' ,
         latlng : new kakao.maps.LatLng(37.51013685499783, 127.06217376752065) , 
@@ -36,7 +38,7 @@ export default function Map(){
     ]);
 
     //기존 참조객체명까지 매번 호출하기 번거로우므로 비구조할당을 통해 현재 Index순번 상태변화에 따라 활성화되고 있는 객체의 key값을 바로 추출
-    const { latlng , markerImg , markerSize , markerPos } = ref_info.current[Index];
+    const { latlng , markerImg , markerSize , markerPos } = ref_info[Index];
 
 //위의 비구조화할당으로 추출한 정보값으로 마커 인스턴스 생성
 
@@ -80,9 +82,6 @@ export default function Map(){
         //생성된 마커인스턴스 setMap 메서드 호출시 위치 인스턴스 값 인수로 전달(바인딩)
         inst_marker.setMap(ref_instMap.current);
 
-        //모든 이벤트문은 무조건 useEffect안쪽에서 호출되어야함
-        //이유 : 브라우저 webAPI를 활용하는 모든 구문(dom/fetch/settime) useEffec안쪽에서 실행됨 (밖에서 호출안됨) 순수자바스크립트는 밖에서 호출 
-        //window 객체에 이벤트 연결(리사이즈마다 지도 가운데 위치 초기화 함수 호출)
         window.addEventListener('resize', initPos)
     }, [Index]); //Index 상태값이 변경될 때마다 변경된 순번 상태값으로 지도 인스턴스 다시 생성해서 화면 갱신
 
@@ -94,7 +93,7 @@ export default function Map(){
 
            <nav className="btnSet">
              <ul className = "branch">
-                 {ref_info.current.map((el, idx)=>(
+                 {ref_info.map((el, idx)=>(
                      //동적으로 li 생성 : 클릭한 li의 순서값 idx로 Index 상태값 변경
                      //컴포넌트 재렌더링되면서 변경된 순번의 정보값으로 지도화면 변경됨
                      <li key={idx} className={idx === Index ? 'on' : '' } onClick = {() => setIndex(idx)}>
