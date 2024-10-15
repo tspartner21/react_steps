4844adfeb898c590e2c8e379fa32dabb77e9420a
tspartner21 <tspartner21@gmail.com>
2024-10-15 17:28:07 +0900

    swiper-slide-active

--- src/components/main/Visual.jsx
+++ src/components/main/Visual.jsx
@@ -1,43 +1,38 @@
-import { useFlickrQuery } from "../../hooks/useFlickr";
-import Pic from "../common/Pic";
+import { useFlickrQuery } from '../../hooks/useFlickr';
+import Pic from '../common/Pic';
 import { Swiper, SwiperSlide } from 'swiper/react';
-/* */
 import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import { EffectCoverflow, Pagination } from 'swiper/modules';
+import { useState } from 'react';
 
-export default function Visual(){
-    const { data } = useFlickrQuery({type : 'mine'});
-	console.log(data);
+export default function Visual() {
+	const [Index, setIndex] = useState(0);
+	const { data } = useFlickrQuery({ type: 'mine' });
+	return (
+		<figure className='visual'>
+			{/* Img titles */}
+			<div className='textBox'>
+				{data?.map((el, idx) => (
+					<h2 key={idx} className={Index === idx ? 'on' : ''}>
+						{el.title.substr(0, 30)}
+					</h2>
+				))}
+			</div>
 
-    return (
-        <figure className='visual'>
-                    <Swiper 
-                    slidesPerView={3} 
-                    spaceBetween={100} 
-                    loop={true}                   
-                    effect={'coverflow'}
-                    coverflowEffect={{
-                    rotate: 50, //패널별 회전각도
-                    stretch: 0, //패널간 당겨짐 정도
-                    depth: 100, //원근감 정도 
-                    modifier: 1, //위 3가지 속성의 중첩강도 비율
-                    slideShadows: true, //패널의 그림자
-                    }}
-                    pagination={true}
-                    modules={[EffectCoverflow, Pagination]}
-                    >
-                        {data?.map((pic, idx)=> {  
-                            // pending인 경우, 옵셔널 체이닝으로 ? 추가하기
-                            if(idx >= 10) return null;
-                            return (
-                                <SwiperSlide key={idx}>
-                                    <Pic src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_z.jpg`} style={{width : '100%', height : '100%'}} shadow/>
-                                </SwiperSlide>
-                            );
-                        })}
-                    </Swiper >
-
-        </figure>
-    );
+			{/* Img Pics */}
+			{/* onSlideChange 이벤트 발생시 내부 순서값 구하는 프로퍼티로 index (loop:x), realIndex (loop: 0) */}
+			<Swiper slidesPerView={3} spaceBetween={100} loop={true} centeredSlides={true} onSlideChange={el => setIndex(el.realIndex)}>
+				{data?.map((pic, idx) => {
+					if (idx >= 10) return null;
+					return (
+						<SwiperSlide key={idx}>
+							{/* swiperSlide요소에는 바로 css모션 스타일 적용 비권장 */}
+							<div className='inner'>
+								<Pic src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`} style={{ width: '100%', height: '100%' }} shadow />
+							</div>
+						</SwiperSlide>
+					);
+				})}
+			</Swiper>
+		</figure>
+	);
 }
